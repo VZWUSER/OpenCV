06da0e36e1df3700b5a5fe64087c4b0290bd0d2e
vzwuser <vzwuser999@gmail.com>
2019-02-21 08:15:21 -0500

    Don't kno what changed

--- findface.py
+++ findface.py
@@ -1,6 +1,31 @@
 import face_recognition
 import cv2
 
+refPt = []
+cropping = False
+
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
+
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -28,8 +53,9 @@ face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
-
+#cv2.setMouseCallback("image", click_and_crop)
 while True:
+
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
