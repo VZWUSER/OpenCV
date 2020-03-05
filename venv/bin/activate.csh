d708c5346530a4a9e9db016bfd5eaa823e85e304
mo <vzwuser999@gmail.com>
2020-03-05 10:42:12 -0500

    MOved venv to opencv folder

--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/mhariri/PycharmProjects/OpenCVTestApp/venv"
+setenv VIRTUAL_ENV "/home/mo/PycharmProjects/OpenCV/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mhariri/PycharmProjects/OpenCVTestApp/venv"
+set -gx VIRTUAL_ENV "/home/mo/PycharmProjects/OpenCV/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(venv) "            
+        if test -n "(venv) "
             printf "%s%s" "(venv) " (set_color normal)
         else
             # ...Otherwise, prepend env
